4f658bfb0ad1dd5669cac0173cafec261053fb40
Pummelhead <pummelhead@gmail.com>
2024-02-21 13:30:42 -0700

    getURLsFromHTML function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const { test, expect } = require('@jest/globals')
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTLM } = require('./crawl.js')
 
 
 test('normalizeURL strip https', () => {
@@ -27,5 +27,72 @@ test('normalizeURL strip http', () => {
     const input = 'http://www.google.com/path'
     const actual = normalizeURL(input)
     const expected = 'www.google.com/path'
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL absolute', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+        <a href="https://www.google.com">
+            Google
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://www.google.com"
+    const actual = getURLsFromHTLM(inputHTMLBody, inputBaseURL)
+    const expected = ["https://www.google.com/"]
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL relative', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+        <a href="/path/">
+            Google
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://www.google.com"
+    const actual = getURLsFromHTLM(inputHTMLBody, inputBaseURL)
+    const expected = ["https://www.google.com/path/"]
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL multiple paths', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+        <a href="https://www.google.com/path1/">
+            Google path 1
+        </a>
+        <a href="/path2/">
+            Google path 2
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://www.google.com"
+    const actual = getURLsFromHTLM(inputHTMLBody, inputBaseURL)
+    const expected = ["https://www.google.com/path1/", "https://www.google.com/path2/"]
+    expect(actual).toEqual(expected)
+})
+
+test('normalizeURL invalid', () => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+        <a href="invalid">
+            Invalid URL
+        </a>
+    </body>
+</html>
+`
+    const inputBaseURL = "https://www.google.com"
+    const actual = getURLsFromHTLM(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
